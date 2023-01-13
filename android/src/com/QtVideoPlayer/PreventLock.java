5ca0a6ee628abeb890e93bd0f86c421750a473c6
JediFaust <omur.kubanychbekov@gmail.com>
2023-01-13 17:56:29 +0600

    added screen lock preventing

--- android/src/com/QtVideoPlayer/PreventLock.java
+++ android/src/com/QtVideoPlayer/PreventLock.java
@@ -2,30 +2,24 @@ package com.QtVideoPlayer;
 import org.qtproject.qt.android.bindings.QtActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
+import android.app.Activity;
 
 public class PreventLock extends QtActivity {
 
-    public static int counter = 0;
-    public int id;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        this.id = counter;
-        Log.d("Something", "Created PreventLockJavaClass object with id: " + this.id);
-        counter++;
         super.onCreate(savedInstanceState);
     }
 
     public void preventLock(boolean flag) {
-        setTurnScreenOn(flag);
-        Log.d("Something", "State changed to: " + flag);
-    }
+        runOnUiThread(() -> {
+            if (flag) { getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); }
+            else { getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); }
+        });
 
-    public String sayHello() {
-        return "NativePickerJavaClass object number: " + id + " say hello :)";
-    }
-
-    public static double multiply(double a, double b) {
-        return  a * b;
+        Log.d("JavaLog", "LockFlag changed to: " + flag);
     }
 }
+
+
--- nativeconnector.cpp
+++ nativeconnector.cpp
@@ -6,20 +6,11 @@
 NativeConnector::NativeConnector(QObject *parent)
     : QObject{parent}
 {
-    qDebug() << "C++ worked on constructor";
+
 }
 
 void NativeConnector::switchLock(bool currentLockState) {
-//    QJniObject nativeObj = QNativeInterface::QAndroidApplication::context();
-    QJniObject nativeObj = QJniObject("com.QtVideoPlayer/PreventLock",
-                                 "(Landroid/content/Context;)V",
-                                 QNativeInterface::QAndroidApplication::context());
-
-
-    qDebug() << "C++ started on switchLock";
+    QJniObject nativeObj = QNativeInterface::QAndroidApplication::context();
 
     nativeObj.callMethod<void>("preventLock", "(Z)V", currentLockState);
-//    qDebug() << nativeObj.callMethod<jstring>("sayHello").toString();
-
-    qDebug() << "C++ ended on switchLock";
 }
